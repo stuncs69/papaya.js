ae0c8906d93dc1c1e69427a692e768054a65430a
stuncs69 <0346436@student.rocvantwente.nl>
2023-09-21 13:58:35 +0200

    clean index.ts

--- index.ts
+++ index.ts
@@ -1,9 +1,4 @@
-import NetCore from "./src/networking";
 import { getPublicFileContents } from "./src/util";
-import fs from "fs";
-import colors from "colors";
-import process from "process";
-import { Settings } from "./src/interfaces";
 import { PapayaConfig, PapayaRoute, PapayaServer } from "./src/classes";
 
 export { PapayaServer, getPublicFileContents, PapayaConfig, PapayaRoute };
