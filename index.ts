5eea3363af7c28d9c92c92e38d5eb990171a5643
stuncs69 <stuncs69@stuncs.dev>
2023-11-10 12:41:21 +0100

    Cleanup code

--- index.ts
+++ index.ts
@@ -1,4 +1,5 @@
 import { renderEJS, renderPublic, extractGetParameters, getPostBody } from "./src/util";
 import { PapayaConfig, PapayaRoute, PapayaServer } from "./src/classes";
 
+// Best implementation that i could find at the moment
 export { PapayaServer, renderEJS, renderPublic, PapayaConfig, PapayaRoute, extractGetParameters, getPostBody };
