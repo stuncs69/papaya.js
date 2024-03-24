f33f1b9ed21d61c9b5cb75ce2d3ca57c0119befa
stuncs69 <0346436@student.rocvantwente.nl>
2024-03-24 20:30:45 +0100

    0.0.19b

--- index.ts
+++ index.ts
@@ -1,5 +1,5 @@
-import { renderEJS, renderPublic, extractGetParameters, getPostBody } from "./src/util";
+import { renderEJS, renderPublic, extractGetParameters, getPostBody, render } from "./src/util";
 import { PapayaConfig, PapayaRoute, PapayaServer } from "./src/classes";
 
 // Best implementation that i could find at the moment
-export { PapayaServer, renderEJS, renderPublic, PapayaConfig, PapayaRoute, extractGetParameters, getPostBody };
+export { PapayaServer, renderEJS, render, renderPublic, PapayaConfig, PapayaRoute, extractGetParameters, getPostBody };
